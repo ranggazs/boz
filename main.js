962b59cf608383a4520d92763203011d6bc572a5
ranggazs <74724682+ranggazs@users.noreply.github.com>
2021-03-05 21:24:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6285241040338'] // Put your number here
+global.owner = ['6282346215555'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
